3854edc5fb0d262483995164e6c5526fac9a0750
zenrui <zcreyes1@up.edu.ph>
2024-05-14 02:49:11 +0800

    fix: properly fetching the details of the user

--- lib/pages/donor_details_page.dart
+++ lib/pages/donor_details_page.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import '../models/user_model.dart' as User;
 import '../providers/user_provider.dart';
+import '../providers/auth_provider.dart';
+
 class DonorDetailsPage extends StatefulWidget {
   const DonorDetailsPage({super.key});
 
@@ -20,10 +23,11 @@ class DonorDetailsPageState extends State<DonorDetailsPage> {
     });
   }
 
-
-
+  
+  auth.User? authUser;
   @override
   Widget build(BuildContext context) {
+    authUser = context.read<UserAuthProvider>().user;
     Stream<QuerySnapshot> userStream = context.watch<UserProvider>().users;
 
     return Scaffold(
@@ -34,8 +38,8 @@ class DonorDetailsPageState extends State<DonorDetailsPage> {
         stream: userStream,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            final users = snapshot.data!.docs.map((doc) => User.fromDocument(doc)).toList();
-            User donor = users.firstWhere((user) => user.name == user!.name && user.type == "donor");
+            final users = snapshot.data!.docs.map((doc) => User.User.fromDocument(doc)).toList();
+            User.User donor = users.firstWhere((user) => authUser?.email == user!.username && user.type == "donor");
             return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
