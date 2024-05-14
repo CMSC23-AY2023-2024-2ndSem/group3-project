2655d9de8a794839da5beda6da3560daae7e9af6
Ray Meshack D. Ragas <rdragas1@up.edu.ph>
2024-05-15 00:49:51 +0800

    feat: Users who wish to sign up as an organization may now upload proof of legitimacy by either choosing from gallery or taking a photo.

--- lib/pages/signuporg_page.dart
+++ lib/pages/signuporg_page.dart
@@ -1,9 +1,12 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 import 'package:week9_authentication/models/user_model.dart';
 import 'package:week9_authentication/providers/user_provider.dart';
 import '../providers/auth_provider.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
 class SignUpOrgPage extends StatefulWidget {
   const SignUpOrgPage({super.key});
@@ -19,6 +22,54 @@ class _SignUpOrgState extends State<SignUpOrgPage> {
   String? password;
   String? address;
   String? contactNumber;
+  List<XFile> imageFile = [];
+  List<String> imageFileUrl = [];
+  List<String> imageUrl = [];
+  bool submitClicked = false;
+
+  Future<void> _takePhoto() async {
+    final ImagePicker picker = ImagePicker();
+    XFile? imageFilePick = await picker.pickImage(source: ImageSource.camera);
+    if (imageFilePick != null) {
+      setState(() {
+        imageFile.add(imageFilePick);
+        imageFileUrl.add(imageFilePick.name);
+      });
+    }
+  }
+
+  Future<void> _pickImageFromGallery() async {
+    final ImagePicker picker = ImagePicker();
+    XFile? imageFilePick = await picker.pickImage(source: ImageSource.gallery);
+    if (imageFilePick != null) {
+      setState(() {
+        imageFile.add(imageFilePick);
+        imageFileUrl.add(imageFilePick.name);
+      });
+    }
+  }
+
+  Future<void> _uploadPhotoToStorage() async {
+    if (imageFile.isEmpty) {
+      return;
+    }
+
+    for (int i = 0; i < imageFile.length; i++) {
+      firebase_storage.Reference ref = firebase_storage.FirebaseStorage.instance
+          .ref()
+          .child(
+              'proofoflegitimacy_photos/${email!}_${i}_${DateTime.now()}.jpg');
+      firebase_storage.UploadTask uploadTask =
+          ref.putFile(File(imageFile[i].path));
+
+      uploadTask.whenComplete(() async {
+        imageUrl.add(await ref.getDownloadURL());
+        print('Image URL: ${imageUrl[i]}');
+      });
+
+      await uploadTask.whenComplete(() => print('Photo uploaded'));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +89,7 @@ class _SignUpOrgState extends State<SignUpOrgPage> {
                   passwordField,
                   addressField,
                   contactNumberField,
+                  uploadImageButton(context),
                   submitButton
                 ],
               ),
@@ -154,10 +206,130 @@ class _SignUpOrgState extends State<SignUpOrgPage> {
         ),
       );
 
+  Widget uploadImageButton(context) => Padding(
+        padding: const EdgeInsets.only(bottom: 30.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                if (!(imageFileUrl.isEmpty && submitClicked) ||
+                    imageFileUrl.isNotEmpty && !submitClicked)
+                  ElevatedButton(
+                      onPressed: () {
+                        uploadImagePopUp(context);
+                      },
+                      child: const Text("Upload Proof/s of Legitimacy")),
+                if (imageFileUrl.isEmpty && submitClicked)
+                  ElevatedButton(
+                      style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll(
+                              Color.fromARGB(255, 180, 139, 138))),
+                      onPressed: () {
+                        uploadImagePopUp(context);
+                      },
+                      child: const Text(
+                        "Upload Proof/s of Legitimacy",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                if (imageFileUrl.isEmpty && submitClicked)
+                  const Text("Please upload proof of legitimacy",
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Color.fromARGB(255, 180, 139, 138)))
+              ],
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  //  uploadImagePopUp(context);
+                  uploadImageContainer(context);
+                },
+                child: const Icon(Icons.image_outlined))
+          ],
+        ),
+      );
+
+  uploadImagePopUp(context) => showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text("Upload options"),
+          actions: [
+            Row(
+              children: [
+                TextButton(
+                    onPressed: () {
+                      _takePhoto();
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Take a photo")),
+                TextButton(
+                    onPressed: () {
+                      _pickImageFromGallery();
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Choose from gallery")),
+              ],
+            )
+          ],
+        ),
+      );
+
+  uploadImageContainer(context) => showDialog(
+      context: context,
+      builder: (context) => LayoutBuilder(
+            builder: (context, constraints) => AlertDialog(
+              scrollable: true,
+              title: const Text("Proof/s of Legitimacy"),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    height: constraints.maxHeight * 0.2,
+                    width: constraints.maxWidth * 0.9,
+                    child: ListView.builder(
+                        itemCount: imageFileUrl.length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: const Icon(Icons.folder_copy_rounded),
+                            title: Text(imageFileUrl[index],
+                                style: const TextStyle(
+                                    decoration: TextDecoration.underline)),
+                            onTap: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (context) => AlertDialog(
+                                          content: SizedBox(
+                                        height: 250,
+                                        child: Image.file(
+                                          File(imageFile[index].path),
+                                          height: 200,
+                                        ),
+                                      )));
+                            },
+                          );
+                        }),
+                  ),
+                ],
+              ),
+              actions: [
+                TextButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: const Text("Confirm"))
+              ],
+            ),
+          ));
+
   Widget get submitButton => ElevatedButton(
       onPressed: () async {
-        if (_formKey.currentState!.validate()) {
+        setState(() {
+          submitClicked = true;
+        });
+
+        if (_formKey.currentState!.validate() && imageFile.isNotEmpty) {
           _formKey.currentState!.save();
+
+          await _uploadPhotoToStorage();
+          print(imageUrl);
 
           bool emailExists = await context
               .read<UserAuthProvider>()
@@ -185,7 +357,7 @@ class _SignUpOrgState extends State<SignUpOrgPage> {
                 contactNumber: contactNumber!,
                 status: false,
                 donations: [],
-                proofs: []);
+                proofs: imageUrl);
             await context
                 .read<UserProvider>()
                 .firebaseService
@@ -195,7 +367,6 @@ class _SignUpOrgState extends State<SignUpOrgPage> {
           // check if the widget hasn't been disposed of after an asynchronous action
           if (mounted) {
             Navigator.pop(context);
-            
           }
         }
       },
